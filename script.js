44ce48e261cac57a40d07b78429eb7f92a6f1556
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 09:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+// Function to play the sound
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // Exit if there is no audio element for the key
+    audio.currentTime = 0; // Rewind the audio to the start
+    audio.play();
+    key.classList.add('playing');
+}
+
+// Function to remove the "playing" class when the transition ends
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // Skip if it's not a transform transition
+    this.classList.remove('playing');
+}
+
+// Add event listeners
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
